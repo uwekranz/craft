891e17e089065a947db4ebdf509e018fbb7c1bf1
Uwe Kranz <uwe.karsten.kranz@gmail.com>
2014-02-08 12:22:58 +0100

    delete unused code

--- main/java/userinterface/UserInterface.java
+++ main/java/userinterface/UserInterface.java
@@ -1,10 +1,7 @@
 package userinterface;
 
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.swing.JFrame;
 
@@ -19,45 +16,14 @@ public class UserInterface extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	public UserInterface(GameView gameView) {
-		super();
-		setContentPane(gameView);
-		setSize(DEFAULT_SCREEN_SIZE);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
 	public void display() {
 		setVisible(true);
 		GameView gameView = (GameView) getContentPane();
 		gameView.paintComponent(getGraphics());
 	}
 
-	public Boolean hasComponentOfType(Class<? extends TypedJComponent> type) {
-		List<TypedJComponent> typedComponents = this.getTypedComponents();
-		return typedComponents.contains(type);
-	}
-
 	public Boolean hasContentPaneOfType(Class<? extends TypedJComponent> type) {
 		return ((TypedJComponent) getContentPane()).getSubClass() == type;
-	}
-
-	private List<TypedJComponent> getTypedComponents() {
-		List<Component> components = getComponentsOfContentPane();
-		components.add(getContentPane());
-		return castComponentIntoTypedComponents(components);
-	}
-
-	private List<TypedJComponent> castComponentIntoTypedComponents(List<Component> components) {
-		List<TypedJComponent> typedComponents = null;
-		for (Component component : components) {
-			typedComponents.add((TypedJComponent) component);
-		}
-		return typedComponents;
-	}
-
-	private List<Component> getComponentsOfContentPane() {
-		Component[] components = getContentPane().getComponents();
-		return Arrays.asList(components);
 	}
 
 	public GameView getCageGraphic() {
