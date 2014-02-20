9b67cfe2331183b90151563a87a89efed0abdf43
Uwe Kranz <uwe.karsten.kranz@gmail.com>
2014-02-20 21:03:55 +0100

    clean SnakeTest

--- test/java/model/SnakeTest.java
+++ test/java/model/SnakeTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import static model.Direction.RIGHT;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
@@ -12,39 +13,40 @@ import org.junit.Test;
 public class SnakeTest {
 
 	private Snake snake;
-	int snakesInitialLength;
+	int theSnakesInitialLength;
 
 	@Before
 	public void setUp() {
 		snake = new Snake();
-		snakesInitialLength = snake.getLength();
+		theSnakesInitialLength = snake.getLength();
 	}
 
 	@Test
 	public void itShouldHaveLengthOne_OnCreation() {
-		assertThat(snakesInitialLength, is(1));
+		assertThat(theSnakesInitialLength, is(1));
 	}
 
 	@Test
 	public void itShouldGetLonger_WhenItEats() {
 		snake.eat();
-		int snakesCurrentLength = snake.getLength();
+		int theSnakesLengthAfterEating = snake.getLength();
 
-		assertThat(snakesInitialLength, is(lessThan(snakesCurrentLength)));
+		assertThat(theSnakesInitialLength, is(lessThan(theSnakesLengthAfterEating)));
 	}
 
 	@Test
-	public void itShouldBeLocated_AtPoint0And0_OnCreation() {
-		Point snakesLocation = snake.getLocation();
+	public void theSnakesInitialLocation_IsTheOrigin() {
+		Point theSnakesInitialLocation = snake.getLocation();
+		Point theOrigin = new Point(0, 0);
 
-		assertThat(snakesLocation, is(new Point(0, 0)));
+		assertThat(theSnakesInitialLocation, is(theOrigin));
 	}
 
 	@Test
 	public void itShouldBeMoving_ToTheRight_OnCreation() {
-		Direction snakesDirectionOfMovement = snake.getDirectionOfMovement();
+		Direction theSnakesDirectionOfMovement = snake.getDirectionOfMovement();
 
-		assertThat(snakesDirectionOfMovement, is(Direction.RIGHT));
+		assertThat(theSnakesDirectionOfMovement, is(RIGHT));
 	}
 
 	@Test
