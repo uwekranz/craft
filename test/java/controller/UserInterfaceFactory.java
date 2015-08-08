c953294819305a9c693d16dc624a3f5b42b3bacc
Uwe Kranz <uwe.karsten.kranz@gmail.com>
2015-08-08 08:48:07 +0200

    take care of todo: find a place for constant

--- test/java/controller/UserInterfaceFactory.java
+++ test/java/controller/UserInterfaceFactory.java
@@ -3,20 +3,21 @@ package controller;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import model.GameModel;
+import model.Snake;
 import model.SnakeLocation;
 import view.UserInterface;
 
 public class UserInterfaceFactory {
 
 	public static UserInterface createMock() {
-		Controller controller = mock(Controller.class);
 		GameModel gameModel = mock(GameModel.class);
-		// TODO: 01.6.2015, find a place for this "constant"
-		int bodyLength = 10;
-		when(gameModel.getSnakeLocation()).thenReturn(new SnakeLocation(bodyLength));
+		SnakeLocation snakeLocation = new SnakeLocation(Snake.INITIAL_BODY_LENGTH);
+		when(gameModel.getSnakeLocation()).thenReturn(snakeLocation);
+
+		Controller controller = mock(Controller.class);
 		when(controller.getGameModel()).thenReturn(gameModel);
-		UserInterface userInterface = new UserInterface(controller);
-		return userInterface;
+
+		return new UserInterface(controller);
 	}
 
 }
