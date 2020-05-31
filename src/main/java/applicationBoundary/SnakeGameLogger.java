e02f2023c82e384e653e665e616858650b1d352d
Uwe Kranz <uwe.karsten.kranz@gmail.com>
2020-05-31 09:25:30 +0200

    fix compile error, remove unused imports

--- src/main/java/applicationBoundary/SnakeGameLogger.java
+++ src/main/java/applicationBoundary/SnakeGameLogger.java
@@ -1,14 +1,8 @@
 package applicationBoundary;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.config.Configurator;
 
 public class SnakeGameLogger {
-
-	public SnakeGameLogger() {
-		Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.DEBUG);
-	}
 
 	public static void debug(Object classToLogFrom, String message) {
 		LogManager.getLogger(classToLogFrom.toString()  + classToLogFrom).debug(message);
