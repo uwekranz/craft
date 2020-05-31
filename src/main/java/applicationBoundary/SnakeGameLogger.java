c0c2bda725f6f1466d582c5a6667112c7f6939ed
Uwe Kranz <uwe.karsten.kranz@gmail.com>
2020-05-31 09:13:34 +0200

    let logger write to console

--- src/main/java/applicationBoundary/SnakeGameLogger.java
+++ src/main/java/applicationBoundary/SnakeGameLogger.java
@@ -1,14 +1,22 @@
 package applicationBoundary;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class SnakeGameLogger {
 
+	public SnakeGameLogger() {
+		Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.DEBUG);
+	}
+
 	public static void debug(Object classToLogFrom, String message) {
-		LogManager.getLogger(classToLogFrom).debug(message);
+		LogManager.getLogger(classToLogFrom.toString()  + classToLogFrom).debug(message);
+		System.out.println(message);
 	}
 
 	public static void info(Object classToLogFrom, String message) {
 		LogManager.getLogger(classToLogFrom).info(message);
+		System.out.println(classToLogFrom.toString() + message);
 	}
 }
